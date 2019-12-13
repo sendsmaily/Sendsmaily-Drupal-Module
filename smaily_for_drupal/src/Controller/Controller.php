8e1bc4e3e1cd981c2882e7918f8178a2392bdfd3
TomAbel <tomkristian.abel@tptlive.ee>
2019-12-13 13:49:42 +0200

    Made failure page error messages translateable

--- smaily_for_drupal/src/Controller/Controller.php
+++ smaily_for_drupal/src/Controller/Controller.php
@@ -31,8 +31,26 @@ class Controller extends ControllerBase {
    */
   public function failure() {
     $response_bool = isset($_GET['code']) && isset($_GET['message']);
-    if ($response_bool && $_GET['code'] !== 101) {
-      $this->messenger()->addStatus(Html::escape($_GET['message']));
+    if ($response_bool) {
+      switch ((int) $_GET['code']) {
+        case 201:
+          $this->messenger()->addWarning($this->t('Data must be posted with POST method.'));
+          break;
+
+        case 204:
+          $this->messenger()->addWarning($this->t('Input does not contain a valid email address.'));
+          break;
+
+        case 205:
+          $this->messenger()->addWarning($this->t(
+            'Could not add to subscriber list for an unknown reason. Probably something in Smaily.'
+          ));
+          break;
+
+        default:
+          $this->messenger()->addError($this->t('Something went wrong. Try again later'));
+          break;
+      }
     }
     return [
       '#markup' => '<p>' . $this->t('You have not been added to subscribers list.') . '</p>',
